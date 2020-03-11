0f26b61671fc3d9240824d95d5588ca52067ae0e
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-03-10 20:16:08 -0400

    Update main.c

--- snek_API/main.c
+++ snek_API/main.c
@@ -4,44 +4,24 @@
 #include <unistd.h>
 #include <time.h>
 
-void play_game() {
-    randSeeding(time(0));
-    //printf("starting\n");
-	//board initialized, struct has pointer to snek
-	GameBoard* board = init_board();
-	//show_board(board);
-
-	int axis = AXIS_INIT;
-	int direction = DIR_INIT;
-	
-	int play_on = 1;
-	int coord[2];
-	
-	while (play_on){
-		game_step(coord, &axis, &direction, board);
-	}
-	end_game(&board);
-
-}
-
 int game_step(int* axis, int* direction, GameBoard* board) {
-		int coord[2];
+	int coord[2];
 
-		coord[x] = board->snek->head->coord[x];
-		coord[y] = board->snek->head->coord[y];
+	coord[x] = board->snek->head->coord[x];
+	coord[y] = board->snek->head->coord[y];
 
-		struct stack* steps = random_search_cant_die(board);
+	struct stack* steps = random_search_cant_die(board);
 
-		while (steps->size>0){
-		    struct step* best_step = pop(steps);
-		    *axis = best_step->axis;
-		    *direction = best_step->direction;
-		    free(best_step);
-		}
+	while (steps->size>0){
+		struct step* best_step = pop(steps);
+		*axis = best_step->axis;
+		*direction = best_step->direction;
+		free(best_step);
+	}
 
-		delete_stack(steps);
+	delete_stack(steps);
 
-		return(advance_frame(*axis, *direction, board));
+	return(advance_frame(*axis, *direction, board));
 }
 
 int main(){
