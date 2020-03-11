16e7a66985fcaed6ca3631c3c62ba9f4c4643a3c
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-03-10 20:15:13 -0400

    Repackaging

--- snek_API/main.c
+++ snek_API/main.c
@@ -18,76 +18,30 @@ void play_game() {
 	int coord[2];
 	
 	while (play_on){
-		coord[x] = board->snek->head->coord[x];
-		coord[y] = board->snek->head->coord[y];
-		/*
-		unsigned short go_x = (axis == AXIS_Y && direction == 1 && coord[y] == (BOARD_SIZE - 1)) || (axis == AXIS_Y && direction == -1 && coord[y] == 0);
-		unsigned short go_y = (axis == AXIS_X && direction == 1 && coord[x] == (BOARD_SIZE - 1)) || (axis == AXIS_X && direction == -1 && coord[x] == 0);
-		if (go_x) {
-			// going to hit the bottom or top
-			// go right or left
-			axis = AXIS_X;
-			if (coord[x] < (int)(BOARD_SIZE / 2)){ 
-				direction = RIGHT;
-			} else {
-				direction = LEFT;
-			}
-		} else if (go_y) {
-			axis = 	AXIS_Y;
-			if (coord[y] < (int)(BOARD_SIZE / 2)){ 
-				direction = DOWN;
-			} else {
-				direction = UP;
-			}
-		}*/
-		//struct stack* steps = tree_search(board, get_score());
-		///*
-		//printf("begin search");
-		struct stack* steps = random_search_cant_die(board);
-		//printf("end search");
-		while (steps->size>0){
-		    struct step* best_step = pop(steps);
-		    axis = best_step->axis;
-		    direction = best_step->direction;
-		    //printf("Axis: %d, Direction: %d  ||  ",axis,direction);
-		    free(best_step);
-		}
-		//printf("\n");
-		delete_stack(steps);//*/
-		//show_board(board);
-		//printf("Cloning board");
-		//board = clone_board(board);
-		//board->snek = clone_snek(board->snek);
-		//printf("Done cloning");
-		int old_flag = board->moogleFlag;
-		play_on = advance_frame(axis, direction, board);
-		int new_flag = board->moogleFlag;
-		if (old_flag == 0 && new_flag == 1){
-		    //printf("FOOD SPAWNED\n");
-		}
-
-        /*
-		printf("Going ");
-
-		if (axis == AXIS_X){
-			if (direction == RIGHT){
-				printf("RIGHT");
-			} else {
-				printf("LEFT");
-			}
-		} else {
-			if (direction == UP){
-				printf("UP");
-			} else {
-				printf("DOWN");
-			}
-		} printf("\n");*/
-
-		//usleep(1000);
+		game_step(coord, &axis, &direction, board);
 	}
-	//show_board(board);
 	end_game(&board);
 
+}
+
+int game_step(int* axis, int* direction, GameBoard* board) {
+		int coord[2];
+
+		coord[x] = board->snek->head->coord[x];
+		coord[y] = board->snek->head->coord[y];
+
+		struct stack* steps = random_search_cant_die(board);
+
+		while (steps->size>0){
+		    struct step* best_step = pop(steps);
+		    *axis = best_step->axis;
+		    *direction = best_step->direction;
+		    free(best_step);
+		}
+
+		delete_stack(steps);
+
+		return(advance_frame(*axis, *direction, board));
 }
 
 int main(){
